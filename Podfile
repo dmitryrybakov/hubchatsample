d0e4db02787cae64afcabc7c10d5f25a606e6473
Rybakov Dmytro <dmitry.rybakov@srost-studio.com>
2017-02-03 23:10:43 +0200

    Initial commin

---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+def common_pods
+    pod 'Alamofire', '~> 4.3'
+
+end
+
+target :'hubchatTestapp' do
+	common_pods
+end
+
---
+++ hubchatTestapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hubchatTestapp
+//
+//  Created by Dmitry on 03.02.17.
+//  Copyright © 2017 hubchat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
